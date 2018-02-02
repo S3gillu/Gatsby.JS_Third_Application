181872a003e52fec68bc64beb165bd882d464b5a
S3gillu <shubhamsaurabh440@yahoo.com>
2018-02-02 17:40:58 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+    <div>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!  What do I like to do? Lots of course but definitely enjoy building
+            websites.
+        </p>
+    </div>
+);
