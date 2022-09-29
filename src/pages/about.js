1bebd0f733b14f94b5e1a4daa89b43d6ba908c55
Shubham Saurabh <31066063+S3gillu@users.noreply.github.com>
2022-09-29 12:50:01 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
     <div>
-        <h1>About Me</h1>
+        <h1>About Me SHubham</h1>
         <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
     </div>
 )
